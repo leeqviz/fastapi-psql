c1b066ff1f252669a8e07e9582e8e3d6ea879ce0
leeqviz <leeqviz@gmail.com>
2026-04-18 12:12:08 +0300

    small fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,4 @@
-import pytest
-from alembic import command
-from alembic.config import Config
+import pytest_asyncio
 from httpx import ASGITransport, AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
@@ -10,45 +8,46 @@ from src.models import Base
 
 TEST_DATABASE_URL = "postgresql+asyncpg://user:password@localhost:5432/test_db"
 
-engine_test = create_async_engine(TEST_DATABASE_URL, echo=False)
-async_session_maker_test = async_sessionmaker(
-    engine_test, class_=AsyncSession, expire_on_commit=False
-)
+engine = create_async_engine(TEST_DATABASE_URL, echo=False)
+session_maker = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 
-@pytest.fixture(scope="session", autouse=True)
-def run_migrations():
+""" @pytest_asyncio.fixture(scope="session")
+def run_db_migrations():
     alembic_cfg = Config("alembic.ini")
     alembic_cfg.set_main_option("sqlalchemy.url", TEST_DATABASE_URL)
 
     command.upgrade(alembic_cfg, "head")
     yield
-    command.downgrade(alembic_cfg, "base")
+    command.downgrade(alembic_cfg, "base") """
 
 
-@pytest.fixture(autouse=True, scope="session")
-async def prepare_database():
-    async with engine_test.begin() as conn:
+@pytest_asyncio.fixture(scope="session", loop_scope="session", autouse=True)
+async def setup_db():
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     yield
-    async with engine_test.begin() as conn:
+    async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture
-async def session():
-    async with async_session_maker_test() as session:
+@pytest_asyncio.fixture
+async def get_db_session():
+    async with session_maker() as session:
         yield session
         await session.rollback()
 
 
-@pytest.fixture
-async def client(session):
-    def override_get_session():
-        yield session
+@pytest_asyncio.fixture
+async def client(db_session):
+    def override_get_db_session():
+        yield db_session
 
-    app.dependency_overrides[psql_conn.get_session] = override_get_session
+    app.dependency_overrides[psql_conn.get_session] = override_get_db_session
+
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as ac:
         yield ac
+
+    app.dependency_overrides.clear()
